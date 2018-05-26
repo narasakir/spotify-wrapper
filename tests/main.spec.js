f14c84390c78d70f10fa80b5b04fbce7105a664a
Raphael Narasaki <narasakir@gmail.com>
2018-05-26 15:20:40 -0300

    intall sinon and other dependecies

---
+++ tests/main.spec.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main'
+
+describe('Spotify Wrapper', () => {
+
+  describe('Smoke Test', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist
+    })
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist
+    })
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist
+    })
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist
+    })
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist
+    })
+  })
+
+  describe('Generic Search', () => {
+
+    it('should call fetch function', () => {
+      const artists = search()
+    })
+
+
+  })
+
+
+
+})
